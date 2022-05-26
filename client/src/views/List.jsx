9ded970adbe5ec0d531c0da96903669d17e5d5d6
EmilRR24 <grimlei2005@yahoo.com>
2022-05-25 14:22:19 -1000

    group project updated with css and list view layout

--- client/src/views/List.jsx
+++ client/src/views/List.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Slide from '../components/Slide'
 import { Link, useNavigate } from "react-router-dom";
 
 const List = () => {
   const navigate = useNavigate();
 
+  const [games, setGames] = useState([]);
 
   return (
     <div>
@@ -26,6 +28,13 @@ const List = () => {
       <div className="body">
         <div className="bodyLeft"></div> {/*OUTSIDE BORDER DON'T USE THIS DIV*/}
         <div className="bodyCenter">
+          <div>
+          <img style={{width: '50%'}} src="https://images.adsttc.com/media/images/5ef1/68f8/b357/6529/f500/0347/large_jpg/yc8m6r-A_jpeg.jpg?1592879340" alt="" />
+          {games.map(game =>(
+            games.box_art
+          ))}
+          </div>
+          <Slide />
         </div>
           <div className="bodyRight"></div> {/*OUTSIDE BORDER DON'T USE THIS DIV*/}
       </div>
